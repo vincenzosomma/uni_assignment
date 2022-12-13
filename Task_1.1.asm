4aef4249aa429c91ce5c2367264cb14cabdfa17f
Vi. So <77669617+vincenzosomma@users.noreply.github.com>
2022-12-13 09:58:07 +0000

    Update Task_1.1.asm

--- Task_1.1.asm
+++ Task_1.1.asm
@@ -1,7 +1,9 @@
+
 section .text
 global _start
 
 _start:
+
 	mov ecx, prompt
 	mov edx, prompt_len
 	call printstring
@@ -11,30 +13,36 @@ _start:
 	je f_woman
 	cmp al,'m'
 	je f_man
+
 error:
+
 	mov ecx,werror
 	mov edx,error_len
 	call printstring
 	jmp done
 
 f_man:
+
 	mov ecx,man
 	mov edx,man_len
 	call printstring
 	jmp done
 
 f_woman:
+
 	mov ecx,woman
 	mov edx,woman_len
 	call printstring
 	jmp done	
 
 done:
+
 	mov ebx,0
 	mov eax,1
 	int 0x80
 
 getAnsware:
+
 	push ecx
 	mov ebx,0
 	mov eax,3
@@ -45,18 +53,23 @@ getAnsware:
 	ret
 
 printstring:
+
 	mov ebx,1
 	mov eax,4
 	int 0x80
 	ret
 
 section .data
-	prompt db "Male or Woman?",0xa,"Use man or woman",0xa
+
+	prompt db "Are you a Male or Woman?",0xa,"Type: man or woman",0xa
 	prompt_len equ $-prompt
+	
 	man db "Hi Sir!!",0xa
 	man_len equ $-man
+	
 	woman db "Hi Madam!!",0xa
 	woman_len equ $-woman
+	
 	werror db "WRONG ANSWARE TRY AGAIN!!!", 0xa
 	error_len equ $-werror
 	
